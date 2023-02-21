b5259d9cfc35436abf1ef985ef34aeaad1339277
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-21 08:12:13 +0800

    Installed react-router and enabled routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([{
+   path: "/",
+   element:<div>Home Page</div>
+},
+{
+  path: "/test",
+  element:<div>Home Test</div>
+},
+]);
+
+
+
 function App() {
   return (
     <div>
-      Hello World!
+      <RouterProvider router={router}/>
     </div>
-  )
-}
+  );
+}  
+
 
 export default App;
